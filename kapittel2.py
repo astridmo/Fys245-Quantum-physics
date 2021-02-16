3a4cf67f8a84b6607910c5f265f3aed12f7fb5e3
astridmo <astrid.moum@gmail.com>
2021-02-16 09:24:38 +0100

    Phi works

--- kapittel2.py
+++ kapittel2.py
@@ -10,24 +10,28 @@ __email__ = 'astridmo@nmbu.no'
 
 import math as m
 import cmath
+import numpy as np
+
+h = 6.63 * 10 ** (-34)  # Plancks constant
 
 
-sigma = 0.01
-t = 4
-h = 6.63 * 10**(-34)  # Plancks constant
-mass = 0.0000000008
+def phi(sigma, t, x, mass, v):
+    """Calculate wave function"""
+    p0 = mass * v
+    comp_var = complex(0, (t * h) / mass)
+
+
+    phi = (m.sqrt(sigma) / (m.pi ** (1 / 4) * cmath.sqrt(sigma ** 2 + comp_var))) * cmath.exp(
+        (-(x - (p0 * t) / mass) ** 2) / (2 * sigma ** 2 + comp_var)) * cmath.exp(complex(0, (k0 * x - w0 * t)))
+
+    return phi
+
+sigma = 0.01 * 10 ** (-2)
+t = 5
+mass = 9.1094 * 10 ** (-31)  # Mass of electron
 x = 2
 v = 10
-p0 = mass * v
-comp_var = complex(0, (t*h)/mass)
 k0 = 3
 w0 = 4
-#phi = (m.sqrt(sigma)/m.pi**(1/4) * m.sqrt(sigma**2 + complex(0, (t*h)/m))) #* m.exp(-(x-(p0*t))/m)
-phi = (m.sqrt(sigma)/(m.pi**(1/4) * cmath.sqrt(sigma**2 + comp_var))) * cmath.exp((-(x-(p0*t)/mass)**2)/(2 * sigma**2 * comp_var)) * cmath.exp(complex(0,(k0*x - w0*t)))
-print(phi)
 
-t = 1
-h = 2
-m = 4
-print(cmath.sqrt(sigma**2 + complex(0, (t*h)/m)))
-#print(math.sqrt(3))
+print(phi(sigma, t, x, mass, v))
