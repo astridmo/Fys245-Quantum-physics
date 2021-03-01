d69ce814f8644100849d6f1c9f7290c0ba58dc54
astridmo <astrid.moum@gmail.com>
2021-03-01 11:40:15 +0100

    Start on tilleggsoppgave 1

--- kapittel4.py
+++ kapittel4.py
@@ -10,17 +10,22 @@ __email__ = 'astridmo@nmbu.no'
 
 import math
 import matplotlib.pyplot as plt
+from numpy import linspace
 
 a = 2
-h_bar = 1.05457 * 10 ^ (-34)
-m_elec = 9.10938356 * 10 ^ (-31)
+h_bar = 1.05457
+peis = 1 * 10 ** (-34)
+m_elec = 9.10938356 * 10 ** (-31)
 
 E = 10  # energy of ??
 
 tsi = math.sqrt(2 * m_elec * E) * a * (1 / (h_bar * 2))
-tsi0 = 10
+tsi0 = linspace(0, 15)
 # tsi0 = (a/2) * math.sqrt(2*m_elec*V0)/h_bar
 
 tan_tsi = math.tan(tsi)
+
+plt.plot(tsi0, tsi)
+plt.show()
 
 
