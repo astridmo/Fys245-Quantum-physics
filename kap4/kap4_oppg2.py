c8a1ab3ce56bf5848e33de156e27f9acb85be16e
astridmo <astrid.moum@gmail.com>
2021-03-08 11:58:21 +0100

    add T_E_larger_than_v0 method

--- kap4/kap4_oppg2.py
+++ kap4/kap4_oppg2.py
@@ -9,28 +9,25 @@ __author__ = 'Astrid Moum'
 __email__ = 'astridmo@nmbu.no'
 
 import numpy as np
-import math
 
-a = 5 * 10**(-5)  # m
+a = 5 * 10 ** (-5)  # m
 V0 = 5  # eV
-m_elec = 0.109 * 10**(-31)  # kg
-h_bar = 6.582 * 10**(-16)  # eV*s
-
+m_elec = 0.109 * 10 ** (-31)  # kg
+h_bar = 6.582 * 10 ** (-16)  # eV*s
 
 
 def T_E_less_than_V0(m_elec, V0, E, a):
     """Transmission from task 4.26 for when E < V0"""
     k = np.sqrt(2 * m_elec * E) / h_bar
     kappa = np.sqrt(2 * m_elec * (V0 - E)) / h_bar
-    return (1 + (((k**2 + kappa**2)**2) * (np.sinh(kappa*a)**2))/(4*(k**2)*(kappa**2)))
+    return (1 + (((k ** 2 + kappa ** 2) ** 2) * (np.sinh(kappa * a) ** 2)) / (4 * (k ** 2) * (kappa ** 2))) ** (-1)
 
 
 def T_E_larger_than_v0(m_elec, V0, E, a):
     """Transmission from task 4.28 for when E > V0"""
     k = np.sqrt(2 * m_elec * E) / h_bar
-    k0 = np.sqrt(2 * m_elec * (E-V0)) / h_bar
-    return (1 + (((k**2 - k0**2)**2) * (np.sin(k0*a)**2))/(4*(k**2)*(k0**2)))
-
+    k0 = np.sqrt(2 * m_elec * (E - V0)) / h_bar
+    return (1 + (((k ** 2 - k0 ** 2) ** 2) * (np.sin(k0 * a) ** 2)) / (4 * (k ** 2) * (k0 ** 2))) ** (-1)
 
 
 E = np.arange(0.1, 3, 0.001)
